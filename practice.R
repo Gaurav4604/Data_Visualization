91ade5a0bb2539cc74af9c4f3f65470ebc113011
Gaurav Singh <gp5in9h@gmail.com>
2021-02-22 12:57:39 +0530

    Practice

---
+++ practice.R
@@ -0,0 +1,35 @@
+library(ggplot2)
+
+data(diamonds)
+
+head(diamonds)
+
+hist(diamonds$carat, main = "Carat Histogram", xlab = "Carat")
+# frequency histogram of level ofcCarat
+
+plot(price ~ carat, data = diamonds)
+# scatter plot between [price (y)] , [carat (x)]
+
+boxplot(diamonds$carat, main = "Carat Histogram", ylab = "Carat")
+# frequency boxplot of level of carat represented by y-axis
+
+# using ggplot
+ggplot(data = diamonds) + geom_histogram(aes(x = carat))
+# geom_histogram where x-axis is carat data
+# from diamonds dataset, and y axis is frequency
+
+ggplot(data = diamonds) + geom_density(aes(x = carat), fill = "grey50")
+
+g <- ggplot(diamonds)
+# point graph
+g + geom_point(aes(x = carat, y = price))
+# point graph with color
+g + geom_point(aes(x = carat, y = price, color = color))
+# point graph with individual analysis
+g + geom_point(aes(x = carat, y = price, color = color)) + facet_wrap(~color)
+# point graph with level wise analysis via cut and clarity parameters
+g + geom_point(aes(x = carat, y = price, color = color)) + facet_grid(cut~clarity)
+# box plot
+ggplot(diamonds, aes(y = carat, x = 1)) + geom_boxplot()
+# box plot with multiple x parameters
+ggplot(diamonds, aes(y = carat, x = cut)) + geom_boxplot()
